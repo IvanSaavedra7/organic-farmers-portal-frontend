28122054f29ac55d974ba31253ad17df7453f9a4
IvanSaavedra7 <ip280103@gmail.com>
2024-12-29 03:54:48 -0300

    ajustes deploy versão angular 16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
