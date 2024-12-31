606d24d2cf1be00d03a3f20fdbcb8bc0d930d7b4
IvanSaavedra7 <ip280103@gmail.com>
2024-12-30 21:54:51 -0300

    headers + primeira seção home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    MainHeaderComponent 
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  userLocation: string = '';
+  locationState: 'initial' | 'success' | 'error' = 'initial';
+
+  ngOnInit() {
+    // Não iniciamos a solicitação de localização automaticamente
+  }
+
+  requestLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => this.handleLocationSuccess(position),
+        (error) => this.handleLocationError(error)
+      );
+    } else {
+      this.locationState = 'error';
+    }
+  }
+
+  private handleLocationSuccess(position: GeolocationPosition) {
+    const { latitude, longitude } = position.coords;
+    this.reverseGeocode(latitude, longitude);
+  }
+
+  private handleLocationError(error: GeolocationPositionError) {
+    console.error('Erro ao obter localização:', error);
+    this.locationState = 'error';
+  }
+
+  private reverseGeocode(latitude: number, longitude: number) {
+    // Aqui você deve usar um serviço de geocodificação reversa
+    // Como exemplo, vamos usar a API do Nominatim do OpenStreetMap
+    fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`)
+      .then(response => response.json())
+      .then(data => {
+        const address = data.address;
+        console.log(data)
+        this.userLocation = `${address.city || address.town}, ${address.state}, ${address.suburb || address.neighbourhood}`;
+        this.locationState = 'success';
+      })
+      .catch(error => {
+        console.error('Erro na geocodificação reversa:', error);
+        this.locationState = 'error';
+      });
+  }
+}
